3ce4bb5c04297d853e6ce2ca4c722024f6f49345
Prem Thatikonda <premthatikonda@Prems-MacBook-Air.local>
2023-08-23 14:58:47 +0530

    factorial finder

---
+++ factorial.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+    int fact = 1;
+
+    printf("Enter a number : ");
+    scanf("%d",&num);
+
+    for(int i=num; i > 0; i--)
+    {
+        fact = fact * i;
+    }
+
+    printf("Factorial is %d", fact);
+}
